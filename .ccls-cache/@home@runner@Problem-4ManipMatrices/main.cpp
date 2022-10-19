3f4532bde815daadb6d0d554132c3c51051b30b3
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-10-19 15:18:38 +0000

    input all of matrix data

---
+++ .ccls-cache/@home@runner@Problem-4ManipMatrices/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+
+  const int N_ROWS = 10;
+  const int N_COLS = 3;
+  int AirQuality_Index[N_ROWS][N_COLS] = {
+  {50, 68, 58}, {58, 115, 78}, {62, 110, 81}, {66 , 124, 110}, {64, 152, 121}, {46, 143, 90}, {46, 143, 90}, {25, 91, 91}, {27, 19, 59}, {17, 13, 27}};
+
+  
+  
+  
+
+}
